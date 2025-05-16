4b9c0ea34c631e07f6054f1ecee217d4bf746e93
bharatroy4D <bharatroy1005@gmail.com>
2025-05-16 17:19:32 +0600

    topbar componment aded

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import TopBar from '../Components/TopBar/TopBar';
+import Navbar from '../Components/Navbar/Navbar';
+
 
 const Root = () => {
     return (
         <div>
             <TopBar></TopBar>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
